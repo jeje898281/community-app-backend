33b0fd507ffec6308e5c7c415463fa38c7f661c5
jeje898281 <ctkho0828@gmail.com>
2025-05-26 16:02:15 +0800

    feat: add QRcode generator API

--- src/routes/meetingRoutes.js
+++ src/routes/meetingRoutes.js
@@ -1,9 +1,12 @@
 // src/routes/reportRoutes.js
 const router = require('express').Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const { handleCheckin, handleGetAttendanceSummary } = require('../controllers/meetingController');
+const {
+    handleCheckin, handleGetAttendanceSummary, handleGenerateQRCodes
+} = require('../controllers/meetingController');
 
 router.post('/checkin', authMiddleware, handleCheckin);
 router.get('/summary/:meetingId', authMiddleware, handleGetAttendanceSummary);
+router.post('/generate-qr-codes', authMiddleware, handleGenerateQRCodes);
 
 module.exports = router;
