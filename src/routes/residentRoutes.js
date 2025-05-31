9d3bd665a94af30b043988cd9eff0ce4a27b2295
jeje898281 <ctkho0828@gmail.com>
2025-06-01 01:02:51 +0800

    feat: add create resident API

--- src/routes/residentRoutes.js
+++ src/routes/residentRoutes.js
@@ -1,8 +1,9 @@
 // src/routes/residentRoutes.js
 const router = require('express').Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const { getResidents } = require('../controllers/residentController');
+const { getResidents, handleCreateResident } = require('../controllers/residentController');
 
 router.get('/', authMiddleware, getResidents);
+router.post('/', authMiddleware, handleCreateResident);
 
 module.exports = router;
